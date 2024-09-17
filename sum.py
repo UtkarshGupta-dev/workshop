bf30a0fe1044b5d7e9200eaf377a4b4ef25d6342
Utkarsh Gupta <utkarsh41005@gmail.com>
2024-09-17 17:40:11 +0530

    gbyg

--- sum.py
+++ sum.py
@@ -2,10 +2,11 @@
 num1 = 40
 num2 = 10
 num3 = 90
+num4 = 100
 
 # Adding two nos
-sum = num1 + num2 + num3
+sum = num1 + num2 + num3 + num4
 product = num3*num2
 # printing values
-print("Sum of", num1, "and", num2 , "is", sum)
+print("Sum of", num1, ",", num2 , ",",num3,"and",num4,"is", sum)
 print(product)
